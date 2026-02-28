9ed12f9047f73475db90f3686e329da4e8e1d9dd
Enzo Sagun <enzosagun@enzos-macbook-pro.local>
2026-02-28 16:24:10 -0600

    ready for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
   server: {
     proxy: {
       "/agents": {
@@ -11,7 +14,7 @@ export default defineConfig({
       },
     },
     watch: {
-      ignored: ["**/worker-configuration.d.ts", "**/.wrangler/**"]
-    }
-  }
+      ignored: ["**/worker-configuration.d.ts", "**/.wrangler/**"],
+    },
+  },
 });
